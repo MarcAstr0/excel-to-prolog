27edac5ad3201545ce0a15b72b20f93098a4c1c6
Mario Castro Squella <mario.castro.squella@gmail.com>
2017-11-13 16:29:02 -0300

    Added a function to get the row contents.

--- src/main/scala/com/marcastr0/ExcelReader.scala
+++ src/main/scala/com/marcastr0/ExcelReader.scala
@@ -1,12 +1,13 @@
 package com.marcastr0
 
 import org.apache.poi.xssf.usermodel.{XSSFCell, XSSFSheet, XSSFWorkbook}
+import org.apache.poi.ss.usermodel.Cell.{CELL_TYPE_NUMERIC, CELL_TYPE_STRING}
 
 object ExcelReader {
 
   def getSheets(workbook: XSSFWorkbook): List[XSSFSheet] = {
     val sheets = for {
-      i <- 0 to workbook.getNumberOfSheets - 1
+      i <- 0 until workbook.getNumberOfSheets
     } yield workbook.getSheetAt(i)
     sheets.toList
   }
@@ -14,9 +15,26 @@ object ExcelReader {
   def getSheetHeaders(sheet: XSSFSheet): List[XSSFCell] = {
     val headers = sheet.getRow(0)
     val cells = for {
-      i <- 0 to headers.getPhysicalNumberOfCells - 1
+      i <- 0 until headers.getPhysicalNumberOfCells
     } yield headers.getCell(i)
     cells.toList
+  }
+
+  def getRows(sheet: XSSFSheet): List[List[Any]] = {
+    val rows = for {
+      i <- 1 until sheet.getPhysicalNumberOfRows
+    } yield sheet.getRow(i)
+    rows.map { row => {
+        for {
+          i <- 0 until row.getPhysicalNumberOfCells
+        } yield {
+          row.getCell(i).getCellType match {
+            case CELL_TYPE_STRING => row.getCell(i).toString
+            case _ => row.getCell(i).getNumericCellValue
+          }
+        }
+      }.toList
+    }.toList
   }
 
 }
--- src/test/scala/com/marcastr0/ExcelReaderSpec.scala
+++ src/test/scala/com/marcastr0/ExcelReaderSpec.scala
@@ -1,24 +1,26 @@
 package com.marcastr0
 
+import java.io.InputStream
+
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.scalatest.FlatSpec
 
 class ExcelReaderSpec extends FlatSpec {
-  val xlsxFile = getClass.getResourceAsStream("/family.xlsx")
+  val xlsxFile: InputStream = getClass.getResourceAsStream("/family.xlsx")
   val workbook = new XSSFWorkbook(xlsxFile)
 
   "getSheets" should "get every sheet in the Excel file" in {
     val sheets = ExcelReader.getSheets(workbook)
     assert(sheets.length == 3)
-    assert(sheets(0).getSheetName() == "parent")
-    assert(sheets(1).getSheetName() == "male")
-    assert(sheets(2).getSheetName() == "female")
+    assert(sheets.head.getSheetName == "parent")
+    assert(sheets(1).getSheetName == "male")
+    assert(sheets(2).getSheetName == "female")
   }
 
   "getSheetHeaders" should "return the header cells of the sheet" in {
     val sheets = ExcelReader.getSheets(workbook)
     assert(
-      ExcelReader.getSheetHeaders(sheets(0)).map(cell => cell.toString)
+      ExcelReader.getSheetHeaders(sheets.head).map(cell => cell.toString)
         == List("parentName", "childName")
     )
     assert(
@@ -29,5 +31,28 @@ class ExcelReaderSpec extends FlatSpec {
       ExcelReader.getSheetHeaders(sheets(2)).map(cell => cell.toString)
         == List("name")
     )
+  }
+
+  "getRows" should "return the rows as lists of strings" in {
+    val sheets = ExcelReader.getSheets(workbook)
+    assert(
+      ExcelReader.getRows(sheets.head) ==
+        List(List("david", "john"), List("john", "eliza"), List("suzie", "eliza"))
+    )
+    assert(
+      ExcelReader.getRows(sheets(1)) ==
+        List(List("david"), List("john"))
+    )
+    assert(
+      ExcelReader.getRows(sheets(2)) ==
+        List(List("eliza"), List("suzie"))
+    )
+  }
+
+  it should "return the rows as lists of strings and numbers" in {
+    val xlsxFile: InputStream = getClass.getResourceAsStream("/age.xlsx")
+    val workbook = new XSSFWorkbook(xlsxFile)
+    val sheets = ExcelReader.getSheets(workbook)
+    assert(ExcelReader.getRows(sheets.head) == List(List("mario", 35.0)))
   }
 }
