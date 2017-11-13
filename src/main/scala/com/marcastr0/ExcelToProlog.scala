43459f18f0a88b2424729fa5f38075f527ca3a77
Mario Castro Squella <mario.castro.squella@gmail.com>
2017-11-13 17:39:32 -0300

    Program successfully generates .pl files.

--- src/main/scala/com/marcastr0/ExcelToProlog.scala
+++ src/main/scala/com/marcastr0/ExcelToProlog.scala
@@ -1,11 +1,31 @@
 package com.marcastr0
 
+import java.io.{File, FileInputStream, InputStream, PrintWriter}
+
+import org.apache.poi.xssf.usermodel.{XSSFSheet, XSSFWorkbook}
+
 object ExcelToProlog {
   def main(args: Array[String]): Unit = {
     if (args.length == 0) {
       println("Must receive an Excel file as input.")
     } else {
       val excelFile = args(0)
+      val fileStream: InputStream = new FileInputStream(excelFile)
+      val workbook = new XSSFWorkbook(fileStream)
+      val sheets = ExcelReader.getSheets(workbook)
+      for (sheet <- sheets) {
+        val writer = new PrintWriter(new File(sheet.getSheetName + ".pl"))
+        writer.write(createKnowledgeBase(sheet))
+        writer.close()
+      }
     }
+  }
+
+  def createKnowledgeBase(sheet: XSSFSheet): String = {
+    val header = PrologWriter.writeExplanation(sheet.getSheetName, ExcelReader.getSheetHeaders(sheet)) + "\n"
+    val rows = ExcelReader.getRows(sheet) map {
+      row => PrologWriter.writeFact(sheet.getSheetName, row)
+    }
+    header + rows.mkString("\n")
   }
 }
---
+++ src/test/scala/com/marcastr0/ExcelToPrologSpec.scala
@@ -0,0 +1,20 @@
+package com.marcastr0
+
+import java.io.InputStream
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.scalatest.FlatSpec
+
+class ExcelToPrologSpec extends FlatSpec {
+  val xlsxFile: InputStream = getClass.getResourceAsStream("/family.xlsx")
+  val workbook = new XSSFWorkbook(xlsxFile)
+  val sheets = ExcelReader.getSheets(workbook)
+
+  "createKnowledgeBase" should "create a knowledge base of facts" in {
+    assert(ExcelToProlog.createKnowledgeBase(sheets.head) ==
+      """|% parent(parentName,childName).
+        |parent(david,john).
+        |parent(john,eliza).
+        |parent(suzie,eliza).""".stripMargin)
+  }
+}
